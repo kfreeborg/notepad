3a48a72a30de9eaeda9a3b128bd94b004bcccf1e
Kelsey Freeborg <kelsey.freeborg@gmail.com>
2020-12-10 19:51:13 -0700

    html read

--- server.js
+++ server.js
@@ -1,1 +1,22 @@
-console.log("I am working");
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.get('/api/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, './Develop/db/db.json'));
+});
+
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, './Develop/public/notes.html'));
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './Develop/public/index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Now serving port ${PORT}!`);
+});
