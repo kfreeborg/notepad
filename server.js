3ce943cf3fedfbbacdb6d9e39642effad188df62
Kelsey Freeborg <kelsey.freeborg@gmail.com>
2020-12-10 20:56:01 -0700

    post is working

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+let notes = [];
+
 app.get('/api/notes', (req, res) => {
   res.sendFile(path.join(__dirname, './Develop/db/db.json'));
 });
@@ -15,6 +20,13 @@ app.get('/notes', (req, res) => {
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, './Develop/public/index.html'));
+});
+
+app.post('/api/notes', (req, res) => {
+  const newNote = req.body;
+  console.log(newNote);
+  notes.push(newNote);
+  res.json(newNote);
 });
 
 app.listen(PORT, () => {
