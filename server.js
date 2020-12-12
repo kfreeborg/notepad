9423c9dbd9ade27f0a81acf2bbc9450594ee3cda
Kelsey Freeborg <kelsey.freeborg@gmail.com>
2020-12-11 18:53:43 -0700

    push to heroku

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(path.join(__dirname, './Develop/public')));
 
 app.get('/api/notes', (req, res) => {
   res.sendFile(path.join(__dirname, './Develop/db/db.json'));
